4cc7dbadcc82d35dae77d1691d3639671ad65822
oleg-shevelev <oleg.shevelyov@schibsted.com>
2018-02-11 10:34:34 +0000

    Small code optimizations and improvements in the dashboard

--- src/html_generator.py
+++ src/html_generator.py
@@ -13,6 +13,12 @@ div.main {background:white;width:1024px;right: 0;left: 0;margin-right: auto;marg
 """
 
 
+def _add_tag_sequence(tag_method, text_method, tag: str, texts: list) -> None:
+    for text in texts:
+        with tag_method(tag):
+            text_method(text)
+
+
 def _build_leaf(tag_method, text_method, leaf: DashboardNode) -> None:
     day_goal_secs = leaf.day_goal * 60 if leaf.day_goal is not None else 0
     week_goal_secs = leaf.week_goal * 60 if leaf.week_goal is not None else 0
@@ -22,24 +28,22 @@ def _build_leaf(tag_method, text_method, leaf: DashboardNode) -> None:
     with tag_method("tr"):
         with tag_method("td"):
             with (tag_method("a", href=leaf.link) if leaf.link is not None else EmptyContext()):
-                with (tag_method("strike") if leaf.day_time >= day_goal_secs > 0 else EmptyContext()):
+                with (tag_method("strike")
+                      if leaf.day_time >= day_goal_secs > 0 or leaf.week_time >= week_goal_secs > 0
+                      else EmptyContext()):
                     text_method(leaf.name)
 
         last_time_str = leaf.last_time.strftime("%a %d %b  %H:%M") if leaf.last_time is not None else "-"
-        days_this_week_str = [weekdays_str[day] for day in leaf.week_days]
+        days_this_week_list = [weekdays_str[day] for day in leaf.week_days]
+        if len(days_this_week_list) > 0:
+            days_this_week_str = "{}: {}".format(len(days_this_week_list), ", ".join(days_this_week_list))
+        else:
+            days_this_week_str = "-"
 
-        with tag_method("td"):
-            text_method(secs_to_str(leaf.day_time))
-        with tag_method("td"):
-            text_method(day_goal_str)
-        with tag_method("td"):
-            text_method(secs_to_str(leaf.week_time))
-        with tag_method("td"):
-            text_method(week_goal_str)
-        with tag_method("td"):
-            text_method(", ".join(days_this_week_str) if len(days_this_week_str) > 0 else "-")
-        with tag_method("td"):
-            text_method(last_time_str)
+        _add_tag_sequence(tag_method, text_method, "td",
+                          [secs_to_str(leaf.day_time), day_goal_str,
+                           secs_to_str(leaf.week_time), week_goal_str,
+                           days_this_week_str, last_time_str])
 
 
 def _build_node(doc, tag_method, text_method, node: DashboardNode, path: str) -> None:
@@ -52,20 +56,9 @@ def _build_node(doc, tag_method, text_method, node: DashboardNode, path: str) ->
         with tag_method("table"):
             if node.have_children_stats():
                 with tag_method('tr'):
-                    with tag_method('th'):
-                        text_method("Name")
-                    with tag_method('th'):
-                        text_method("Today")
-                    with tag_method('th'):
-                        text_method("Daily goal")
-                    with tag_method('th'):
-                        text_method("This week")
-                    with tag_method('th'):
-                        text_method("Weekly goal")
-                    with tag_method('th'):
-                        text_method("Days this week")
-                    with tag_method('th'):
-                        text_method("Last time")
+                    _add_tag_sequence(tag_method, text_method, "th",
+                                      ["Name", "Today", "Daily goal", "This week",
+                                       "Weekly goal", "Days this week", "Last time"])
             for child in node.children:
                 _build_node(doc, tag_method, text_method, child,
                             path="{} {}".format(path + " -> " if len(path) > 0 else "", node.name))
